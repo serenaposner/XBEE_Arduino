44ca6b0a974184b4efb3f0cbe2cd04d680bd491b
serenaposner <serenaposner@gmail.com>
2015-03-11 14:31:40 -0400

    xbeearduino

---
+++ XBEE_arduino.ino
@@ -0,0 +1,21 @@
+//Arduino Code:
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  int voltage = sensorValue * (  50/1023.0);
+  // print out the value you read:
+  Serial.print(voltage);
+  Serial.print(",");
+  Serial.println(voltage);
+
+}
